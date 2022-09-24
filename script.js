e216adb40f4735f62a3dd89ea7fd7446f82efb58
marbar <mariluvv23@gmail.com>
2022-09-24 10:56:56 -0400

    quiz is functional

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var startButton = document.getElementById("start-button")
 var questionContainerElement = document.getElementById("question-container")
 var questionElement = document.getElementById('question')
-var answerButtonsElement = document.getElementById('answer-buttons')
-var starterContainerEl = document.getElementById('starter-container')
+// reach into the html to grab the group of buttons
+var answerButtonsElement = document.getElementById('answer-buttons') // grabed the html el (div)
+// grab the buttons in the group of buttons
+var choiceButtons = answerButtonsElement.children; // return an array of html el
+// add an event listener for each button
 
-var shuffledQuestions, randomQuestionIndex
+var starterContainerEl = document.getElementById('starter-container')
+var currentQuestionIndex;
+
+
+var timerElement = document.querySelector(".timer")
+var timerCount
 
 startButton.addEventListener('click', startQuiz)
 
@@ -12,57 +20,107 @@ function startQuiz() {
     startButton.classList.add('hide')
     starterContainerEl.classList.add('hide')
     questionContainerElement.classList.remove('hide')
-    randomQuestionIndex = Math.floor(Math.random() * 4)
-    currentQuestionIndex = 0
+    currentQuestionIndex = 0;
+    timerCount = 60
+    startTimer()
     nextQuestion()
 
 }
 
 function nextQuestion() {
-    showQuestion(questions[randomQuestionIndex])
+    // if we're on the 2nd question
+    if (currentQuestionIndex > 0) {
+        // ** eval if the question was correct
+        // grab the correct answer 
+        var lastQuestionIndex = currentQuestionIndex - 1;
+        var lastQuestion = questions[lastQuestionIndex];
+        var lastQuestionAnswer = lastQuestion.answer;
+    }
+    // if we're one the last question
+    if (currentQuestionIndex >= 4) {
+        return;
+    }
+    var currentQuestion = questions[currentQuestionIndex];
+    questionElement.innerText = currentQuestion.question
+    // console.log(choiceButtons)
+    // for each choice in choices
+    for (index = 0; index < currentQuestion.choices.length; index++) {
+        choiceButtons[index].textContent = currentQuestion.choices[index]
+        choiceButtons[index].value = currentQuestion.choices[index]
+    }
 }
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    var choiceButtons = answerButtonsElement.children
-    for (index = 0; index < choiceButtons.length; index++) {
-        choiceButtons[index].textContent = question.choices[index].text
-        choiceButtons[index].value = question.choices[index].text
-        //Click event below console logs each button value to the console.
-        //choiceButtons[index].addEventListener('click', function (event) {
-           // console.log(event.target.value)
-       // }) 
-   }
-   choiceButtons.addEventListener('click', nextQuestion) 
-  
+for (index = 0; index < choiceButtons.length; index++) {
+    choiceButtons[index].addEventListener('click', checkAnswer)
 }
 
-function selectAnswer() {
-nextQuestion()
+function checkAnswer(event) {
+    var userChoice = event.target.textContent;
+    var correctAnswer = questions[currentQuestionIndex].answer
 
+
+    if (userChoice != correctAnswer) {
+        timerCount -= 10
+    }
+    // go next question after we check
+    currentQuestionIndex = currentQuestionIndex + 1;
+    if (currentQuestionIndex < questions.length) {
+        nextQuestion()
+    }
+}
+
+function startTimer() {
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            timerElement.textContent = timerCount + '';
+        }
+        if (timerCount <= 0 || currentQuestionIndex >= questions.length) {
+            //timerElement.textContent = 'Out of Time!';
+            clearInterval(timer);
+            endQuiz();
+        }
+    }, 1000)
+}
+
+function endQuiz() {
+    questionContainerElement.classList.add('hide')
+    document.getElementById("end-container").classList.remove('hide')
+    document.getElementById("scoreText").textContent = timerCount
 }
 
 var questions = [
     {
         question: 'Arrays in JavaScript can be used to store ___.',
         answer: 'all of the above',
-        choices: [{ text: 'Numbers and Strings' }, { text: 'other Arrays' }, { text: 'Booleans' }, { text: 'all of the above' }]
+        choices: ['Numbers and Strings', 'other Arrays', 'Booleans', 'all of the above']
     },
     {
         question: 'Commonly used data types do NOT include ___.',
         answer: 'Alerts',
-        choices: [{ text: 'Strings' }, { text: 'Booleans' }, { text: 'Alerts' }, { text: 'numbers' }]
+        choices: ['Strings', 'Booleans', 'Alerts', 'numbers']
     },
     {
         question: 'You can adjust how your application appears on various screens by using ___.',
         answer: 'Media Queries',
-        choices: [{ text: 'for loops' }, { text: 'Media Queries' }, { text: 'HTML' }, { text: 'JavaScript' }]
+        choices: ['for loops', 'Media Queries', 'HTML', 'JavaScript']
     },
     {
         question: 'JavaScript goes in which HTML element?',
         answer: '<script>',
-        choices: [{ text: '<div>' }, { text: '<header>' }, { text: '<script>' }, { text: '<footer>' }]
+        choices: ['<div>', '<header>', '<script>', '<footer>']
     }
 
 
 ]
+
+function saveScoreToLocal() {
+    var score = timerCount;
+    var userInitials = document.getElementById("initials").value
+
+    console.log(score, userInitials)
+    localStorage.setItem("example", "yo")
+}
+
+document.getElementById("saveScore").addEventListener("click", saveScoreToLocal)
